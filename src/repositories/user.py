695ae6da2a4ebeca89bc2551edd2d1c2bb15a2e7
Austin Nieset <ajnieset@gmail.com>
2025-01-11 19:50:00 +0200

    implemented User Repository

---
+++ src/repositories/user.py
@@ -0,0 +1,6 @@
+from advanced_alchemy.repository import SQLAlchemyAsyncRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models import User
+
+class UserRepository(SQLAlchemyAsyncRepository):
+    model_type = User
--- src/routers/users.py
+++ src/routers/users.py
@@ -4,6 +4,7 @@ from litestar import Router, get, post
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models import User
+from src.repositories import UserRepository
 
 @dataclass
 class UserCreate:
@@ -13,24 +14,18 @@ class UserCreate:
 
 @get("/")
 async def get_users(db_session: AsyncSession) -> list[User]:
-    stmt = select(User)
-    result = await db_session.execute(stmt)
-    users = result.scalars().all()
-    return users
+    repo = UserRepository(session=db_session)
+    return repo.list()
 
 @get("/{user_id:int}")
-async def get_user(user_id:int, db_session: AsyncSession) -> User:
-    stmt = select(User).where(User.id == user_id)
-    result = await db_session.execute(stmt)
-    user = result.scalars().first()
-    return user
+async def get_user(user_id: int, db_session: AsyncSession) -> User:
+    repo = UserRepository(session=db_session)
+    return repo.get(user_id)
 
 @post("/")
 async def create_user(data: UserCreate, db_session: AsyncSession) -> User:
-    new_user = User(**asdict(data))
-    db_session.add(new_user)
-    await db_session.flush()
-    return new_user
+    repo = UserRepository(session=db_session)
+    return await repo.add(User(**asdict(data)))
 
 
 user_router = Router("/users", route_handlers=[get_users, create_user, get_user])
