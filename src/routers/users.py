8dd131a6182feeec828d387ca6895c21f3dafe55
Austin Nieset <ajnieset@gmail.com>
2025-01-12 10:50:45 +0200

    added delete endpoint

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,6 +1,6 @@
 from dataclasses import asdict, dataclass
 
-from litestar import Router, get, post
+from litestar import Router, delete, get, post
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models import User
@@ -16,13 +16,13 @@ class UserCreate:
 @get("/")
 async def get_users(db_session: AsyncSession) -> list[User]:
     repo = UserRepository(session=db_session)
-    return repo.list()
+    return await repo.list()
 
 
 @get("/{user_id:int}")
 async def get_user(user_id: int, db_session: AsyncSession) -> User:
     repo = UserRepository(session=db_session)
-    return repo.get(user_id)
+    return await repo.get(user_id)
 
 
 @post("/")
@@ -31,4 +31,10 @@ async def create_user(data: UserCreate, db_session: AsyncSession) -> User:
     return await repo.add(User(**asdict(data)))
 
 
-user_router = Router("/users", route_handlers=[get_users, create_user, get_user])
+@delete("/{user_id:int}")
+async def delete_user(user_id: int, db_session: AsyncSession) -> None:
+    repo = UserRepository(session=db_session)
+    await repo.delete(user_id)
+
+
+user_router = Router("/users", route_handlers=[get_users, create_user, get_user, delete_user])
